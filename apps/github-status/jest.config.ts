3e6c881796da1ccb0027fdd9829eab3b0f5dbe3c
lourw <56288712+lourw@users.noreply.github.com>
2025-02-17 12:22:04 -0800

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/SsXosfxcOE

---
+++ apps/github-status/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'github-status',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/github-status',
+};
